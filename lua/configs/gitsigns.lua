bb81b58dbb67f2fb7af7cc60f31013e950650342
Jay <julian.graf@projectchaos.ch>
2024-04-02 22:08:01 +0200

    changed signs for gitsigns and added LSP bindings

--- lua/configs/gitsigns.lua
+++ lua/configs/gitsigns.lua
@@ -1,4 +1,12 @@
-require("gitsigns").setup()
+require("gitsigns").setup({
+  signs = {
+    add = { text = "+" },
+    change = { text = "~" },
+    delete = { text = "_" },
+    topdelete = { text = "‾" },
+    changedelete = { text = "~" }
+  }
+})
 
 vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
 vim.keymap.set("n", "<leader>gl", ":Gitsigns toggle_current_line_blame<CR>", {})
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -11,3 +11,22 @@ map("n", "<leader>fm", function()
 end, { desc = "File Format with conform" })
 
 map("i", "jk", "<ESC>", { desc = "Escape insert mode" })
+
+-- LSP Config
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+  callback = function (event)
+    local mapLsp = function(keys, func, desc)
+      vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc})
+    end
+
+    -- Jump to the definition of the word under the cursor.
+    mapLsp('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+    mapLsp('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+    mapLsp('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementations')
+
+    mapLsp('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinitions')
+  end
+})
