e22cc9d22a8ead60c9d1853826f6f18fa9571e00
Jay <jay@projectchaos.ch>
2024-06-14 13:17:39 +0200

    removed gopher.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -155,14 +155,4 @@ return {
     "tpope/vim-fugitive",
     lazy = false
   },
-  {
-    "olexsmir/gopher.nvim",
-    ft = "go",
-    config = function (_, opts)
-      require("gopher").setup(opts)
-    end,
-    build = function ()
-      vim.cmd [[silent! GoInstallDeps]]
-    end
-  }
 }
