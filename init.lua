11fee3dd61b58c830777bef7d32217343bafe1e2
Jay <julian.graf@projectchaos.ch>
2025-01-07 16:06:32 +0100

    add scroll offset

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
 vim.wo.relativenumber = true
 vim.opt.termguicolors = true
+vim.opt.scrolloff = 5
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
